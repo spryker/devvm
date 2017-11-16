0d5c45994786b3663aa27d31c96853ab980af14a
Marek Obuchowicz <marek@korekontrol.eu>
2017-11-16 20:39:57 +0100

    Update tests for nodejs 8 and nvm

--- saltstack/test/spec/server/nodejs_spec.rb
+++ saltstack/test/spec/server/nodejs_spec.rb
@@ -2,10 +2,15 @@ require 'spec_helper'
 
 describe 'nodejs' do
   describe command('/usr/bin/node -v') do
-    its(:stdout) { should include('v6.') }
+    its(:stdout) { should include('v8.') }
   end
 
   describe command('/usr/bin/yarn --version') do
     its(:stdout) { should include('1.') }
   end
+
+  describe file('/opt/nvm/nvm.sh') do
+    it { should be_readable ) }
+  end
+
 end
