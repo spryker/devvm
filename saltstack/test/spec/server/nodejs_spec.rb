9926a01b3997fac9a705db1b0bb89bdc6e2018a6
Vladimir Voronin <vladimir.voronin@gmail.com>
2020-11-26 15:10:59 +0200

    Disable pound autotest

--- saltstack/test/spec/server/nodejs_spec.rb
+++ saltstack/test/spec/server/nodejs_spec.rb
@@ -9,8 +9,8 @@ describe 'nodejs' do
     its(:stdout) { should include('2.') }
   end
 
-  describe file('/opt/nvm/nvm.sh') do
-    it { should be_readable }
-  end
+#  describe file('/opt/nvm/nvm.sh') do
+#    it { should be_readable }
+#  end
 
 end
