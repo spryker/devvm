6bd5eda30ab0008930d64d1ec744e05b56ba48fe
Vladimir Voronin <vladimir.voronin@gmail.com>
2020-12-01 17:00:38 +0200

    Enable all pound autotests

--- saltstack/test/spec/server/pound_spec.rb
+++ saltstack/test/spec/server/pound_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe 'Pound' do
 
   describe service('pound') do
-#    it { should be_enabled }
+    it { should be_enabled }
     it { should be_running }
   end
 
-#  describe port(443) do
-#    it { should be_listening }
-#  end
+  describe port(443) do
+    it { should be_listening }
+  end
 
 end
